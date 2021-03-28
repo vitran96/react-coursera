d279ea5cdcb0e1fadf98d8f926a92d45811459ef
vitran96 <tranuytrieuvi@gmail.com>
2021-03-28 22:59:14 +0700

    refactor MenuComponent

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes'
 
 function App() {
+  const [dishes] = useState(DISHES);
+
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -10,7 +14,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={dishes} />
     </div>
   );
 }
