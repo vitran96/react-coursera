4bbc37a104e7d945d74737a28f3ae88bfefc2d87
vitran96 <tranuytrieuvi@gmail.com>
2021-08-28 07:34:56 +0700

    Dish submit form

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -1,15 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { FormatDateTime } from '../shared/dateTimeHelper'
+import { Breadcrumb, BreadcrumbItem, Button, Card, CardBody, CardImg, CardText, Label, Modal, ModalBody, ModalHeader, Row, Col } from 'reactstrap';
+import { FormatDateTime } from '../shared/dateTimeHelper';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
 
 function RenderComments({ comments }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values));
+    toggleModal();
+  };
+
   const renderedComments = comments.map(comment => (<RenderComment commentObj={comment} />));
   return (
-    <div className="col-12 col-md m-1">
-      <h4>Comments</h4>
-      {renderedComments}
-    </div>
+    <>
+      <div className="col-12 col-md m-1">
+        <h4>Comments</h4>
+        {renderedComments}
+        <Button outline onClick={toggleModal}>
+          <span className="fa fa-pencil"></span> Submit Comment
+        </Button>
+      </div>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Submit Comment</ModalHeader>
+        <ModalBody>
+          <LocalForm onSubmit={values => onSubmit(values)}>
+            <Row className="form-group mx-1">
+              <Label htmlFor="rating" >Rating</Label>
+              <Control.select model=".rating" name="rating"
+                className="form-control" >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Control.select>
+            </Row>
+            <Row className="form-group mx-1">
+              <Label htmlFor="name" >Your name</Label>
+              <Control.text model=".name" id="name" name="name"
+                className="form-control"
+                placeholder="Your Name"
+                validators={{
+                  required,
+                  minLength: minLength(3),
+                  maxLength: maxLength(15)
+                }} />
+              <Errors className="text-danger"
+                model=".name"
+                show="touched"
+                messages={{
+                  required: 'Required. ',
+                  minLength: 'Must be greater than 2 characters. ',
+                  maxLength: 'Must be less than 15 characters. '
+                }} />
+            </Row>
+            <Row className="form-group mx-1">
+              <Label htmlFor="message">Comment</Label>
+              <Control.textarea model=".message" id="message" name="message"
+                className="form-control"
+                rows="6" />
+            </Row>
+            <Row className="form-group mx-1">
+              <Button type="submit" color="primary">Submit</Button>
+            </Row>
+          </LocalForm>
+        </ModalBody>
+      </Modal>
+    </>
   );
 }
 
