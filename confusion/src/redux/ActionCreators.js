8ade7027c5e6f05e414f3d2a6102e3e79e83a07c
vitran96 <tranuytrieuvi@gmail.com>
2021-09-12 21:48:50 +0700

    refactor

--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -2,25 +2,25 @@ import * as ActionTypes from './ActionTypes';
 import { dishesUrl } from '../shared/baseUrl';
 
 export const fetchDishes = () => dispatch => {
+  const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+  });
+
+  const dishesFailed = errorMessage => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errorMessage
+  });
+
+  const addDishes = dishes => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
+  });
+
   dispatch(dishesLoading(true));
   return fetch(dishesUrl)
     .then(response => response.json())
     .then(dishes => dispatch(addDishes(dishes)));
-}
-
-export const dishesLoading = () => ({
-  type: ActionTypes.DISHES_LOADING
-})
-
-export const dishesFailed = errorMessage => ({
-  type: ActionTypes.DISHES_FAILED,
-  payload: errorMessage
-})
-
-export const addDishes = dishes => ({
-  type: ActionTypes.ADD_DISHES,
-  payload: dishes
-})
+};
 
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
