944df6749a16fd0e7e970948687ddad1cf2f2049
vitran96 <tranuytrieuvi@gmail.com>
2021-09-12 21:53:42 +0700

    add fetching function for Comments and Promotions

--- confusion/src/redux/ActionCreators.js
+++ confusion/src/redux/ActionCreators.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import { dishesUrl } from '../shared/baseUrl';
+import { commentsUrl, dishesUrl, promotionsUrl } from '../shared/baseUrl';
 
 export const fetchDishes = () => dispatch => {
   const dishesLoading = () => ({
@@ -31,3 +31,41 @@ export const addComment = (dishId, rating, author, comment) => ({
     comment: comment
   }
 });
+
+export const fetchComments = () => (dispatch) => {
+  const commentsFailed = (errorMessage) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errorMessage
+  });
+
+  const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+  });
+
+  return fetch(commentsUrl)
+  .then(response => response.json())
+  .then(comments => dispatch(addComments(comments)));
+};
+
+export const fetchPromos = () => (dispatch) => {
+  const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+  });
+
+  const promosFailed = (errorMessage) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errorMessage
+  });
+
+  const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+  });
+
+  dispatch(promosLoading());
+
+  return fetch(promotionsUrl)
+  .then(response => response.json())
+  .then(promos => dispatch(addPromos(promos)));
+}
--- confusion/src/shared/baseUrl.js
+++ confusion/src/shared/baseUrl.js
@@ -1,3 +1,5 @@
 const basePort = 6060;
 export const baseUrl = `http://localhost:${basePort}`;
 export const dishesUrl = `${baseUrl}/dishes`;
+export const commentsUrl = `${baseUrl}/comments`;
+export const promotionsUrl = `${baseUrl}/promotions`;
